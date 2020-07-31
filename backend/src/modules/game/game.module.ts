500b8732d77914296836676e754d377de3433b38
Kristof Komlossy <stuff@kritten.org>
2020-08-01 01:13:14 +0200

    *fixed imports

--- backend/src/modules/game/game.module.ts
+++ backend/src/modules/game/game.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Game } from './game.entity';
 import { GameResolver } from './game.resolver';
 import {GameService} from "./game.service";
-import {CategoryService} from "../category/category.service";
+import {CategoryModule} from "../category/category.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Game]), CategoryService],
-  providers: [GameResolver, GameService, CategoryService],
+  imports: [TypeOrmModule.forFeature([Game]), CategoryModule],
+  providers: [GameResolver, GameService],
 })
 export class GameModule {}
