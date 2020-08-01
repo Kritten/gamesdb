6399b98b7e88620dea9bc3fd6e337cd75ca3dd09
Kristof Komlossy <stuff@kritten.org>
2020-08-01 22:02:22 +0200

    +error if entity not found

--- backend/src/utilities/entity.resolver.ts
+++ backend/src/utilities/entity.resolver.ts
@@ -1,5 +1,6 @@
 import { BaseEntity, BaseInputData, BaseService } from './types';
 import { In } from 'typeorm/index';
+import { NotFoundException } from '@nestjs/common';
 
 export class EntityResolver {
   async handleRelation(
@@ -13,6 +14,16 @@ export class EntityResolver {
         entityData[name].length === 0
           ? []
           : await service.find({ id: In(entityData[name]) });
+
+      if (entity[name].length !== entityData[name].length) {
+        const setIds = new Set(entity[name].map(item => item.id));
+        const entitiesMissing = entityData[name].filter(
+          id => setIds.has(id) === false,
+        );
+        throw new NotFoundException(
+          `${name} with ids ${entitiesMissing} not found`,
+        );
+      }
     }
   }
 }
