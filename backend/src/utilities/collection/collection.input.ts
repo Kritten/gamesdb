57e47e52dfa3a1fc1ba70fb6b4550c63a0e183a3
Kristof Komlossy <stuff@kritten.org>
2020-08-21 09:50:01 +0200

    *fixed collection.input.ts

--- backend/src/utilities/collection/collection.input.ts
+++ backend/src/utilities/collection/collection.input.ts
@@ -1,9 +1,11 @@
-import { InputType } from '@nestjs/graphql';
+import { Field, InputType, Int } from '@nestjs/graphql';
 
 @InputType()
 export class InputCollection {
+  @Field(() => Int)
   page: number;
 
+  @Field(() => Int)
   count: number;
 
   sortBy: string;
