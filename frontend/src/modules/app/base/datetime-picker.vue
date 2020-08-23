5d709e1087a98568f99e9acdc3a5a1351cc69bdc
KKomlossy <kristof.komlossy@bpc.ag>
2020-08-23 20:53:45 +0200

    +datetime-picker

---
+++ frontend/src/modules/app/base/datetime-picker.vue
@@ -0,0 +1,34 @@
+<template>
+  <input
+    v-model="valueInternal"
+    type="datetime-local"
+  >
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+  name: 'DatetimePicker',
+  props: {
+    modelValue: {
+      type: Date,
+      required: true,
+    },
+  },
+  setup(context, { emit }) {
+    const valueInternal = computed({
+      get: () => context.modelValue.toISOString().substring(0, 16),
+      set: (value) => emit('update:modelValue', new Date(value)),
+    });
+
+    return {
+      valueInternal,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
