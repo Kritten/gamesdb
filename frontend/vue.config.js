ec681093eed8727d84dad47f259a22852946a924
Kristof Komlossy <stuff@kritten.org>
2020-08-30 23:18:49 +0200

    *pwa config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,6 +3,29 @@ module.exports = {
   pwa: {
     workboxOptions: {
       skipWaiting: true,
+      clientsClaim: true,
+    },
+    manifestOptions: {
+      name: 'GamesDB',
+      short_name: 'GamesDB',
+      theme_color: '#424242',
+      background_color: '#424242',
+      display: 'standalone',
+      icons: [
+        {
+          src: 'img/icons/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'img/icons/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+    iconPaths: {
+      favicon16: 'img/icons/favicon.ico',
     },
   },
 };
