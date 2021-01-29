71a01dedc65b012af6ca0ea712c445921e3f5e40
Kristof Komlossy <stuff@kritten.org>
2021-01-30 00:36:04 +0100

    +element-plus

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,9 +4,13 @@ import '@/registerServiceWorker';
 import { store } from '@/modules/app/app.store';
 import { router } from '@/modules/app/app.router';
 import { i18n } from '@/modules/app/app.i18n';
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import locale from 'element-plus/lib/locale/lang/de.js';
 
 createApp(App)
   .use(router)
   .use(store)
   .use(i18n)
+  .use(ElementPlus, { locale })
   .mount('#app');
