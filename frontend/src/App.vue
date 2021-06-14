c4a4082f38ee97759b6c2a3b687272ba56e4dcea
Kristof Komlossy <stuff@kritten.org>
2021-06-14 20:08:46 +0200

    *improved app loading

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <router-view v-if="isInitialized" />
   <div v-else>
-    Loading
+    <base-spinner />
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { ServiceApp } from '@/modules/app/app.service';
 import { useApp } from '@/modules/app/composables/useApp';
+import BaseSpinner from '@/modules/app/base/base-spinner.vue';
 
 export default defineComponent({
   name: 'App',
+  components: { BaseSpinner },
   setup() {
     void ServiceApp.initialize();
     const { isInitialized } = useApp();
